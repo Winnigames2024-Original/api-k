bf2fc0ef654d266bcb4c125657440ac38c75012b
link-short <dora.family228@gmail.com>
2025-12-30 18:39:57 +0500

    Create index.js

---
+++ nfc-prikol/random-sound/index.js
@@ -0,0 +1,29 @@
+// Массив путей к вашим аудиофайлам
+const audioFiles = [
+    'https://winnigames2024-original.github.io/api-k/nfc-prikol/bobr-kurva.mp3',
+    'https://winnigames2024-original.github.io/api-k/nfc-prikol/ahhhh-dadyaShasha-AHHHH.mp3',
+    'https://winnigames2024-original.github.io/api-k/nfc-prikol/PONOS.mp3',
+    'https://winnigames2024-original.github.io/api-k/nfc-prikol/pam-pam-pam.mp3' // Добавьте сколько угодно файлов
+];
+
+const playButton = document.getElementById('playButton');
+
+playButton.addEventListener('click', () => {
+    // 1. Получаем случайный индекс
+    const randomIndex = Math.floor(Math.random() * audioFiles.length);
+
+    // 2. Получаем путь к файлу
+    const randomAudioFile = audioFiles[randomIndex];
+
+    // 3. Создаем новый Audio объект
+    const audio = new Audio(randomAudioFile);
+
+    // 4. Воспроизводим звук
+    audio.play()
+        .then(() => {
+            console.log(`Воспроизведен: ${randomAudioFile}`);
+        })
+        .catch(error => {
+            console.error(`Ошибка при воспроизведении: ${error}`);
+        });
+});
