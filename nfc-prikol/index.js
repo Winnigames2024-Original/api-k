6d84f873d793d8d381e5efe935fd2cfb49734ace
link-short <dora.family228@gmail.com>
2025-12-30 18:18:33 +0500

    Update index.js

--- nfc-prikol/index.js
+++ nfc-prikol/index.js
@@ -4,3 +4,18 @@ const sounds_base = urlPPPPPP.get('sounds_base');
 if (sounds_base == "default") {
   loadAndInsertContent('https://winnigames2024-original.github.io/api-k/nfc-prikol/random-sound/', 'content-container');
 }
+
+
+async function loadAndInsertContent(url, targetDivId) {
+  try {
+    const response = await fetch(url);
+    const htmlContent = await response.text(); // Получаем HTML как текст
+
+    const targetDiv = document.getElementById(targetDivId);
+
+    targetDiv.insertAdjacentHTML('beforeend', htmlContent);
+
+    } catch (error) {
+    console.error('Ошибка при загрузке или вставке контента:', error);
+    }
+}
